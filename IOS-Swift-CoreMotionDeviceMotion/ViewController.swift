722a79e07663ee2b99931b8e3e51abae8b8b8dca
Pooya Hatami <pooya@node-1w7jr9qro06swm9kwcwduruqk.ipv6.telus.net>
2018-09-12 23:59:08 -0700

    done

--- IOS-Swift-CoreMotionDeviceMotion/ViewController.swift
+++ IOS-Swift-CoreMotionDeviceMotion/ViewController.swift
@@ -7,19 +7,49 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var xDevi: UITextField!
+    @IBOutlet weak var yDevi: UITextField!
+    @IBOutlet weak var zDevi: UITextField!
+    var motion = CMMotionManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        myDeviceMotion()
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func myDeviceMotion(){
+        print("Start DeviceMotion")
+        motion.deviceMotionUpdateInterval  = 0.5
+        motion.startDeviceMotionUpdates(to: OperationQueue.current!) {
+            (data, error) in
+            print(data as Any)
+            if let trueData =  data {
+                
+                self.view.reloadInputViews()
+                let mPitch = trueData.attitude.pitch
+                let mRoll = trueData.attitude.roll
+                let mYaw = trueData.attitude.yaw
+                self.xDevi!.text = "x (pitch): \(Double(mPitch).rounded(toPlaces: 3))"
+                self.yDevi!.text = "y (roll): \(Double(mRoll).rounded(toPlaces: 3))"
+                self.zDevi!.text = "z (yaw): \(Double(mYaw).rounded(toPlaces: 3))"
+            }
+        }
+        return
     }
-
-
 }
 
+
+extension Double {
+    /// Rounds the double to decimal places value
+    func rounded(toPlaces places:Int) -> Double {
+        let divisor = pow(10.0, Double(places))
+        return (self * divisor).rounded() / divisor
+    }
+}
